0686c2af9a63d1b1c7b10e7885cd789406e0d323
Mustafa-shikalgar <mustafashikalgar2@gmail.com>
2026-07-12 22:36:13 +0530

    Updated chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,11 +1,8 @@
 import sys
 import ollama
-
 prompt = sys.argv[1]
-
 response = ollama.chat(
     model="llama3",
     messages=[{"role": "user", "content": prompt}]
 )
-
 print(response["message"]["content"])
